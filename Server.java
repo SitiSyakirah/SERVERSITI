29511d3a73a3178a9a30618ced36032d8cb0a221
SitiSyakirah <48506122+SitiSyakirah@users.noreply.github.com>
2019-04-11 12:40:06 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,68 +1,31 @@
-//a java program for a Server
-import java.net.*;
 import java.io.*;
-
-public class Server
+import java.net.*;
+class Server
 {
-	//initialize socket and input stream
-	private Socket 		socket = null;
-	private ServerSocket    server = null;
-	private DataInputStream in     = null;
+  public static void main(String[] args) throws Exception
+  {
+      ServerSocket sersock = new ServerSocket(5678);
+      System.out.println("Welcome to AINA'S profile! ");
+      Socket sock = sersock.accept( );                          
+                             
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwriter = new PrintWriter(ostream, true);
 
-	// constructor with port
-	public Server (int port)
-	{
-		//starts server and waits for a connection
-		try
-		{
-			server = new ServerSocket(port);
-			System.out.println("Server started");			
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
 
-			System.out.println("ni hao ma");
-			
-			socket = server.accept();
-			System.out.println("Client accepted");
-			
-			// takes input from the client socket
-			in = new DataInputStream(new 
-BufferedInputStream(socket.getInputStream()));
-
-			String line = "";
-
-		
-	
-			//reads message from client until "Over" is sent
-			while (!line.equals("Over"))
-			{
-				try
-					{
-					
-					line = in.readUTF();		
-					System.out.println(line);
-					}
-			
-					catch(IOException i)
-					{
-						System.out.println(i);
-					}
-			}
-			System.out.println("Closing connection");
-
-			//close connection
-			socket.close();
-			in.close();
-
-		}
-		catch(IOException i)
-
-		{
-			System.out.println(i);
-		}
-	}
-
-public static void main (String args[])
-{
-	Server server = new Server(5000);
-}
-
-}
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
+        {
+           System.out.println(receiveMessage);         
+        }         
+        sendMessage = keyRead.readLine(); 
+        pwriter.println(sendMessage);             
+        pwriter.flush();
+      }               
+    }                    
+}                        
